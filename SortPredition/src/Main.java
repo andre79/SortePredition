0379170b6243d0309e89059fa6cb797c3196e1fb
andre79 <aluizce@gmail.com>
2016-06-07 09:20:32 -0300

    Telas Iniciais

---
+++ SortPredition/src/Main.java
@@ -0,0 +1,80 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnAbrir = new JMenu("Abrir");
+		menuBar.add(mnAbrir);
+		
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		mnAbrir.add(mntmAbrir);
+		
+		JMenuItem mntmSalvar = new JMenuItem("Salvar");
+		mnAbrir.add(mntmSalvar);
+		
+		JMenuItem mntmAtualizarBase = new JMenuItem("Atualizar Base");
+		mnAbrir.add(mntmAtualizarBase);
+		
+		JMenuItem mntmConfiguraes = new JMenuItem("Configurações");
+		mnAbrir.add(mntmConfiguraes);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mnAbrir.add(mntmSair);
+		
+		JMenu mnFormulrios = new JMenu("Formulários");
+		menuBar.add(mnFormulrios);
+		
+		JMenuItem mntmGeraNmero = new JMenuItem("Gera Número");
+		mnFormulrios.add(mntmGeraNmero);
+		
+		JMenuItem mntmGeraureo = new JMenuItem("Gera Áureo");
+		mnFormulrios.add(mntmGeraureo);
+		
+		JMenuItem mntmGeraPrximo = new JMenuItem("Gera Próximo");
+		mnFormulrios.add(mntmGeraPrximo);
+		
+		JMenuItem mntmGeraAleatrio = new JMenuItem("Gera Aleatório");
+		mnFormulrios.add(mntmGeraAleatrio);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
